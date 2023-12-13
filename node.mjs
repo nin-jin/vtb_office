3aa5017518ca99bb55c59669a4eb9756eeaf7d39
nin-jin <nin-jin@users.noreply.github.com>
2023-12-13 07:07:45 +0000

    Deploying to gh-pages from @ nin-jin/vtb_office@65e1590d81070bc1c476353adc7977eff51cf247 🚀

--- node.mjs
+++ node.mjs
@@ -9114,8 +9114,8 @@ var $;
             return [];
         }
         Body_content() {
-            const obj = new this.$.$mol_list();
-            obj.rows = () => this.body();
+            const obj = new this.$.$mol_view();
+            obj.sub = () => this.body();
             return obj;
         }
         body_content() {
@@ -9240,6 +9240,9 @@ var $;
             },
             Body_content: {
                 padding: $mol_gap.block,
+                flex: {
+                    direction: 'column',
+                },
                 justify: {
                     self: 'stretch',
                 },
