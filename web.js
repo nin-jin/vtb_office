a3659f9e078b12c77ea01678957c012bcf22a929
nin-jin <nin-jin@users.noreply.github.com>
2023-12-18 07:08:05 +0000

    Deploying to gh-pages from @ nin-jin/vtb_office@65e1590d81070bc1c476353adc7977eff51cf247 🚀

--- web.js
+++ web.js
@@ -6355,7 +6355,8 @@ var $;
                 title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
-                mol_link_current: this.current()
+                mol_link_current: this.current(),
+                rel: this.relation()
             };
         }
         sub() {
@@ -6389,6 +6390,9 @@ var $;
         }
         current() {
             return false;
+        }
+        relation() {
+            return "";
         }
         event_click(event) {
             if (event !== undefined)
