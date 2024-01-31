ab11571d7e3a52824bdce6869193a9453c87ea2f
nin-jin <nin-jin@users.noreply.github.com>
2024-01-31 07:08:12 +0000

    Deploying to gh-pages from @ nin-jin/vtb_office@65e1590d81070bc1c476353adc7977eff51cf247 🚀

--- node.d.ts
+++ node.d.ts
@@ -2620,15 +2620,8 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_diff_path<Item>(...paths: Item[][]): {
-        prefix: Item[];
-        suffix: Item[][];
-    };
-}
-
-declare namespace $ {
-    class $mol_error_mix extends Error {
-        errors: Error[];
+    class $mol_error_mix extends AggregateError {
+        name: string;
         constructor(message: string, ...errors: Error[]);
         toJSON(): string;
     }
@@ -2636,6 +2629,7 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_data_error extends $mol_error_mix {
+        name: string;
     }
 }
 
